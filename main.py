3c11a1c46b46cdfc1698f81969eaf1e89b7a7769
BrokenButler <castellobutler@gmail.com>
2021-09-19 03:57:29 +0200

    added a ready message to be printed on_ready()

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ bot.add_cog(MusicCog(bot))
 with open('config.json', 'r') as file:
     data = json.load(file)
 
+
+@bot.event
+async def on_ready():
+    print('Anwesend {}'.format(bot.user.name))
+
+
 if __name__ == '__main__':
     bot.run(data['token'])
