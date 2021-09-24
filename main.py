2ff48a3d0092e3352b2e87be9aeaae8d49e08af3
BrokenButler <castellobutler@gmail.com>
2021-09-24 07:38:55 +0200

    changed config.json to token.json
    Signed-off-by: BrokenButler <castellobutler@gmail.com>
    
    Took 1 hour 27 minutes

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import discord
-from discord.ext import commands
 import json
+
+from discord.ext import commands
 
 from musicCog import MusicCog
 
 bot = commands.Bot(command_prefix='-')
 bot.add_cog(MusicCog(bot))
-with open('config.json', 'r') as file:
+
+with open('token.json', 'r') as file:
     data = json.load(file)
 
 
 @bot.event
 async def on_ready():
-    print('Anwesend {}'.format(bot.user.name))
+    print(f'Ready to rumble sir!\n{bot.user.name}')
 
 
 if __name__ == '__main__':
